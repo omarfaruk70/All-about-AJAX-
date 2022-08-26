4238874e357fa05a3ad16f386844f3342fb4222e
omarfaruk70 <faruk1611281892@gmail.com>
2022-08-27 02:12:51 +0600

    class 02 done

---
+++ class 02/script.js
@@ -0,0 +1,18 @@
+// ajax data from external server.
+// in this class we will practise how to get a data from a website under the server.
+// this free website name is icndb.com. its creating for ajax practise purpose.
+// this full process helps api . api stands for application programming interface.
+// api works with a constant link.
+document.getElementById('getData').addEventListener('click', loadjokes)
+function loadjokes(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://api.icndb.com/jokes/random', true)
+    xhr.onload = function(){
+        if(this.status===200){
+            let data = this.responseText;
+            let output = JSON.parse(data);
+            console.log(output)
+        }
+    }
+    xhr.send()
+}
